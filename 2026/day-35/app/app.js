aeecc028df45260c059518ed972090bc5dd77d4d
ArvindJitendraPatil <ubuntu@ip-172-31-18-104.ap-southeast-1.compute.internal>
2026-06-04 18:39:10 +0000

    added : day-35

---
+++ 2026/day-35/app/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Node.js in Docker on EC2!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
